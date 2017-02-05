e2f14ac9a721c0dc5009b66f70e80f84626b92d0
Alexander Bodnia <bodnia.alexander@gmail.com>
2017-02-06 00:16:30 +0200

    fix static path

--- web/server/server.js
+++ web/server/server.js
@@ -5,11 +5,11 @@ var http = require('http');
 var fs = require('fs');
 var app = express();
 app.use(compress());
-var staticFiles = express.static(__dirname + '../build//Client/dist/ru', {maxage: '1h'});
+var staticFiles = express.static(__dirname + '../build/', {maxage: '1h'});
 app.use('/', function(req, res, next){
     staticFiles(req, res, next)
 });
 app.all('/*', function(req, res) {
-    res.sendFile('index.html', { root: __dirname + '/Client/dist/ru' });
+    res.sendFile('index.html', { root: __dirname + '../build/' });
 });
 http.createServer(app).listen(80);
